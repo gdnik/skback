23096a7ce7c859826b94e3005be659f1437e1e13
Тимофей Шмаков <Timofei.schmackov@yandex.ru>
2023-07-20 20:46:11 +0200

    add cors for options

--- src/main/java/Back.java
+++ src/main/java/Back.java
@@ -16,6 +16,30 @@ public class Back {
             return "Skillsy Backend";
         });
 
+        options("/*",
+                (request, response) -> {
+
+                    String accessControlRequestHeaders = request
+                            .headers("Access-Control-Request-Headers");
+                    if (accessControlRequestHeaders != null) {
+                        response.header("Access-Control-Allow-Headers",
+                                accessControlRequestHeaders);
+                    }
+
+                    String accessControlRequestMethod = request
+                            .headers("Access-Control-Request-Method");
+                    if (accessControlRequestMethod != null) {
+                        response.header("Access-Control-Allow-Methods",
+                                accessControlRequestMethod);
+                    }
+
+                    return "OK";
+                });
+
+        before((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "*");
+        });
+
         post("/check/email", (req, res) -> {
             System.out.println("Received POST request");
             Gson gson = new GsonBuilder().create();
