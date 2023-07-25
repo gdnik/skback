d98ef5a1c471d105d54ae3739c8f1a4b1ff6aa22
Тимофей Шмаков <Timofei.schmackov@yandex.ru>
2023-07-25 20:17:21 +0200

    изменение

--- src/main/java/Back.java
+++ src/main/java/Back.java
@@ -11,33 +11,18 @@ public class Back {
 
         //secure("deploy/keystore.jks", "Skilyl#inC@734", null, null);
 
+        options("/*", (request, response) -> "PREFLIGHT REQUEST DONE");
+
+        after((request, response) -> {
+            response.header("Access-Control-Allow-Origin", "http://getflow.me");
+            response.header("Access-Control-Allow-Headers", "Content-Type");
+            response.header("Access-Control-Allow-Methods", "*");
+            response.header("Access-Control-Allow-Credentials", "true");
+        });
+
         get("/", (req, res) -> {
             System.out.println("Skillsy Secure Backend");
             return "Skillsy Backend";
-        });
-
-        options("/*",
-                (request, response) -> {
-
-                    String accessControlRequestHeaders = request
-                            .headers("Access-Control-Request-Headers");
-                    if (accessControlRequestHeaders != null) {
-                        response.header("Access-Control-Allow-Headers",
-                                accessControlRequestHeaders);
-                    }
-
-                    String accessControlRequestMethod = request
-                            .headers("Access-Control-Request-Method");
-                    if (accessControlRequestMethod != null) {
-                        response.header("Access-Control-Allow-Methods",
-                                accessControlRequestMethod);
-                    }
-
-                    return "OK";
-                });
-
-        before((request, response) -> {
-            response.header("Access-Control-Allow-Origin", "*");
         });
 
         post("/check/email", (req, res) -> {
